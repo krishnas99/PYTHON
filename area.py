030a65959039ae1d57e940589afea7f63c680537
krishnas99 <116713282+krishnas99@users.noreply.github.com>
2022-11-04 10:26:37 +0530

    Add files via upload

---
+++ area.py
@@ -0,0 +1,4 @@
+import math
+r=float(input("enter the radius"))
+area=math.pi*r*r
+print("the area of circle is" ,area)
---
+++ prgm1.py
@@ -0,0 +1,15 @@
+print("welcome")
+a=int(input("enter first number"))
+b=int(input("enter the second number"))
+c=a+b
+print("the sum is" ,c)
+d=a*b
+print("the product of a and b" ,d)
+e=a-b
+print("the difference of a and b" ,e)
+f=a/b
+print("the division of a and b is" ,f)
+if a>b:
+    print(a,"is grater than" ,b)
+else:
+    print(b,"is grater than" ,a)
